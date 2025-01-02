6f720655210c5e27d6dd32a87dcc5d862f65d296
Nishkarsh <singhnishkarsh9889@gmail.com>
2025-01-02 17:48:00 +0530

    Changed Api_url

--- Frontend/src/services/ApiService.js
+++ Frontend/src/services/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default class ApiService {
 
-    static BASE_URL = "http://localhost:8080"
+    static BASE_URL = "https://where2stay-production.up.railway.app"
 
     static getHeader() {
         const token = localStorage.getItem("token");
