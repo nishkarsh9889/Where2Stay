dc593ccfddaae0193033062624bc7c9ebdf535fa
Nishkarsh <singhnishkarsh9889@gmail.com>
2025-01-02 17:36:33 +0530

    Updated CorsConfig

--- Backend/src/main/java/com/hotel_app/where2stay/security/CorsConfig.java
+++ Backend/src/main/java/com/hotel_app/where2stay/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://where-2-stay.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
